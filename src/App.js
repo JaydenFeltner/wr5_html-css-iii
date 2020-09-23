74dc3ecb7361d7143f56213c5cb4d36abed07de7
Eric Sellors <github@esellors.com>
2020-09-23 14:11:17 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header';
 
-export default function App() {
-  return (
-    <div className="App">
-      <p>Hello WR5!</p>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      spinning: false
+    }
+  }
+
+  animateSquare = () => {
+    this.setState({ spinning: !this.state.spinning })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <p>Hello WR5!</p>
+
+        {/* animated element - automatically animates*/}
+        <div className='animated-square'></div>
+
+        {/* animated element - on demand animation*/}
+        <div 
+          className={ this.state.spinning ? 'animated-square-ondemand animated-square-ondemand-spin' : 'animated-square-ondemand'}
+          onClick={this.animateSquare}
+        ></div>
+
+        {/* transition circle */}
+        <div className='transition-circle'></div>
+      </div>
+    );
+  }
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            menu: false 
+        }
+    }
+
+    slide = () => {
+        this.setState({ menu: !this.state.menu })
+    }
+
+    render() {
+        return (
+            <div>
+                {/* Navbar */}
+                <nav className='navbar'>
+                    <h1 className='navbar-title'>Start Bootstrap</h1>
+                    <div className='navbar-icon' onClick={this.slide}>&#9776;</div>
+                    <div className='navbar-menu'>
+                        <a href='#'>Home</a>
+                        <a href='#'>About</a>
+                        <a href='#'>Services</a>
+                        <a href='#'>Contact</a>
+                    </div>
+                </nav>
+
+                {/* Slide out menu */}
+                <div className={this.state.menu ? 'menu slide' : 'menu'}>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+                </div>
+            </div>
+        )
+    }
+}
